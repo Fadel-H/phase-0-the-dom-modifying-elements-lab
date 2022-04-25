cd1796e3f854d5ac4a6b459115bb9b38bc5b14d1
Fadel <fadel.g.heji@gmail.com>
2022-04-24 20:50:34 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.setAttribute("id", "victory")
+
+document.body.appendChild(newHeader)
+
+newHeader.innerHTML = "Fadel is the champion"
